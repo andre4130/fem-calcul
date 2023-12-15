a79f5e2d9d10c36ca1f1543b7d9bc960139059e2
André Maia <andre.maia@nemuru.com>
2023-12-15 17:07:17 +0100

    fix

--- app/(main)/(components)/(homeMainContent)/homeMainContent.tsx
+++ app/(main)/(components)/(homeMainContent)/homeMainContent.tsx
@@ -1,14 +1,13 @@
-import styles from './homeMainContent.css';
 export default function HomeMainContent() {
   return (
     <div className="flex flex-col justify-between px-5">
-      <div className={`animate-moveAndFade duration-1000 ${styles.moveAndFade} flex justify-end text-right`}>
+      <div className={`animate-moveAndFade duration-1000  flex justify-end text-right`}>
         <h4 className="w-[50%]">
           FEM Calcul is an engineering consultancy built on decades of experience on computer-aided structural analysis
           covering a broad range of engineering challenges.
         </h4>
       </div>
-      <div className={`max-w-2xl animate-moveAndFade duration-1000 ${styles.moveAndFade}`}>
+      <div className={`max-w-2xl animate-moveAndFade duration-1000 `}>
         <h4>
           It is a company that offers services related with advanced nonlinear finite element analysis of civil
           engineering structures in different sectors such as, bridges, tunnels, energy, historical buildings, etc.
