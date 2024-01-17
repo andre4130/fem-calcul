940b984630c17746f4827e6ae9a7ee642670fffc
André Maia <andre.maia@nemuru.com>
2024-01-17 09:13:47 +0100

    first draft of CV

--- app/(main)/(routes)/cv/page.tsx
+++ app/(main)/(routes)/cv/page.tsx
@@ -1,5 +1,11 @@
+import SectionBanner from '@/components/femcalculComponents/section/components/section-banner';
 import React from 'react';
+import cv from '../../../../public/images/CV.jpg';
 
 export default function CvPage() {
-  return <div>CV page</div>;
+  return (
+    <>
+      <SectionBanner image={cv} title="Curriculum Vitae" divClassName="bg-bottom" />
+    </>
+  );
 }
