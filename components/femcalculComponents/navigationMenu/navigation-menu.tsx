a831228e8255cc730f512c2f83f7f725be8d0790
André Maia <andre.maia@nemuru.com>
2025-02-19 11:00:48 +0100

    feat: :lipstick: Squares animation
    added square animation to logo

--- components/femcalculComponents/navigationMenu/navigation-menu.tsx
+++ components/femcalculComponents/navigationMenu/navigation-menu.tsx
@@ -5,15 +5,19 @@ import Image from 'next/image';
 import NavButton from '@/components/femcalculComponents/nav-button';
 import mesh from '../../../public/images/research.jpg';
 import { usePathname } from 'next/navigation';
+import Squares from '@/components/ui/react-bits/backgrounds/Squares/Squares';
 
 export default function Navigation() {
   const pathname = usePathname();
   const underlinedPathname = 'border-b-[2px] border-b-primary-light transition-all ease-in-out';
 
   return (
-    <NavigationMenu className="hidden md:flex align-middle justify-start w-100  p-4 bg-white w-full border-b-slate-500 border-b-3 b-opacity-50 border-b-[1px]">
+    <NavigationMenu className="hidden md:flex align-middle justify-start w-100 p-4 bg-white w-full border-b-slate-500 border-b-3 b-opacity-50 border-b-[1px]">
       <div className="flex flex-row w-full justify-between items-center">
-        <div>
+        <div className="relative group w-fit">
+          <div className="absolute opacity-0 transition-opacity duration-300 group-hover:opacity-100 h-[44px] w-[220px] bg-transparent">
+            <Squares speed={0.25} squareSize={26} direction="diagonal" borderColor="#3d007f" hoverFillColor="#3d007f" />
+          </div>
           <Link href={'/'} className="flex items-center">
             <Image src="/femlogo.svg" height={40} width={40} alt={'logo'} className=" mr-3" />
             <strong>
